b7fe794249b4da98bb9d9c62926bda15c6543a14
Anthony Ioimo <anthony@kwara.com>
2025-11-16 13:34:30 +0100

    fix bug

--- rules/MajorityStickyBorders.js
+++ rules/MajorityStickyBorders.js
@@ -2,7 +2,7 @@
 import { Majority } from "./Majority.js";
 
 export class MajorityStickyBorders extends Majority {
-  constructor({}) {
+  constructor() {
     super({
       supportBias: 0.0, // just needs to beat uniform
       leadBias: 0.4, // must move 40% of way from p_current to 1.0,
