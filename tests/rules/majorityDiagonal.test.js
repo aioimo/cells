49a1a83e1593a9f8632888cb02ee00281c422140
Anthony Ioimo <anthony@kwara.com>
2025-11-16 13:37:35 +0100

    MajorityDiagonal test fix

--- tests/rules/majorityDiagonal.test.js
+++ tests/rules/majorityDiagonal.test.js
@@ -1,12 +1,13 @@
 import { MajorityDiagonal } from "../../rules/MajorityDiagonal.js";
+import { Matrix } from "../../core/Matrix.js";
 
 describe("MajorityDiagonal Rule", () => {
   test("MajorityDiagonal.nextValue returns expected output for center cell", () => {
-    const inputMatrix = [
+    const inputMatrix = new Matrix([
       ["A", "A", "B"],
       ["A", "A", "A"],
       ["B", "A", "B"],
-    ];
+    ]);
     const rule = new MajorityDiagonal({
       ordering: ["A", "B"],
       gridSize: 3,
