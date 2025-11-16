162afb8f07f27e4b286deeab591dfa8697be013e
Anthony Ioimo <anthony@kwara.com>
2025-11-16 11:35:51 +0100

    reorganise tests

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,16 +1,26 @@
 import { emptyMatrix, mod, random } from "../utils.js";
 
-test("emptyMatrix creates correct shape", () => {
-  const m = emptyMatrix(2, 3);
-  expect(m.length).toBe(2);
-  expect(m[0].length).toBe(3);
-});
+describe("Utils Module", () => {
+  describe("emptyMatrix", () => {
+    test("emptyMatrix creates correct shape", () => {
+      const m = emptyMatrix(2, 3);
+      expect(m.length).toBe(2);
+      expect(m[0].length).toBe(3);
+    });
+  });
 
-test("mod returns positive result", () => {
-  expect(mod(-1, 5)).toBe(4);
-});
+  describe("mod", () => {
+    test("mod returns positive result", () => {
+      expect(mod(-1, 5)).toBe(4);
+      expect(mod(6, 5)).toBe(1);
+      expect(mod(3, 5)).toBe(3);
+    });
+  });
 
-test("random returns element from array", () => {
-  const arr = [1, 2, 3];
-  expect(arr).toContain(random(arr));
+  describe("random", () => {
+    test("random returns element from array", () => {
+      const arr = [1, 2, 3];
+      expect(arr).toContain(random(arr));
+    });
+  });
 });
