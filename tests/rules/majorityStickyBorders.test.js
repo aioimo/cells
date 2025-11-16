5162b80130476b15b3e7549fb6502bc4f266a06c
Anthony Ioimo <anthony@kwara.com>
2025-11-16 13:36:20 +0100

    fix test

--- tests/rules/majorityStickyBorders.test.js
+++ tests/rules/majorityStickyBorders.test.js
@@ -1,8 +1,9 @@
 import { MajorityStickyBorders } from "../../rules/MajorityStickyBorders.js";
+import { Matrix } from "../../core/Matrix.js";
 
 describe("MajorityStickyBorders Rule", () => {
   test("MajorityStickyBorders.nextValue overturns current cell when surrounded by significant majority different value", () => {
-    const inputMatrix = [
+    const inputMatrix = new Matrix([
       ["B", "A", "A", "A", "A", "A", "A"],
       ["B", "B", "A", "A", "A", "A", "A"],
       ["B", "B", "B", "B", "A", "A", "A"],
@@ -10,14 +11,14 @@ describe("MajorityStickyBorders Rule", () => {
       ["B", "B", "B", "B", "B", "B", "A"],
       ["B", "B", "B", "B", "B", "B", "A"],
       ["B", "B", "B", "B", "B", "B", "B"],
-    ];
-    const rule = new MajorityStickyBorders({});
+    ]);
+    const rule = new MajorityStickyBorders();
     const result = rule.nextValue(3, 3, inputMatrix);
     expect(result).toEqual("B");
   });
 
   test("MajorityStickyBorders.nextValue lead value prevents simple majority overturn", () => {
-    const inputMatrix = [
+    const inputMatrix = new Matrix([
       ["B", "A", "A", "A", "A", "A", "A"],
       ["B", "B", "A", "A", "A", "A", "A"],
       ["B", "B", "B", "B", "A", "A", "A"],
@@ -25,8 +26,8 @@ describe("MajorityStickyBorders Rule", () => {
       ["B", "B", "B", "B", "B", "B", "A"],
       ["B", "B", "B", "B", "B", "B", "A"],
       ["B", "B", "B", "B", "B", "B", "A"],
-    ];
-    const rule = new MajorityStickyBorders({});
+    ]);
+    const rule = new MajorityStickyBorders();
     const result = rule.nextValue(3, 3, inputMatrix);
     expect(result).toBeNull();
   });
